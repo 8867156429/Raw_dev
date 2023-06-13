efa2e0fa7611632180d2136b2adde57fffdd7900
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-14 01:52:13 +0530

    Table_3

---
+++ rawcodes/Tables/Folder_2/V1.1.3__Table.sql
@@ -0,0 +1,12 @@
+USE SCHEMA Raw_Dev_Demo;
+CREATE TABLE raw_dev3
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO raw_dev3
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;

c474bac86d85c4601b1302a85874af77f2d0ce50
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-14 01:13:44 +0530

    Second_Table

---
+++ rawcodes/Tables/V1.1.6__Table.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA Raw_Dev-2;
+CREATE TABLE second_table
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO second_table
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;

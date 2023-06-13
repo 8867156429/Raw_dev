25478c3d1850f07559b12f090867483816a52686
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-14 01:11:19 +0530

    First_Table

---
+++ rawcodes/Tables/V1.1.1__Table.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA DEMO_ONE;
+CREATE TABLE Raw_dev
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO Raw_dev
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
